e78410fdb84f175be6b278d97f9aaba16ac78b6e
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-06 13:11:05 +0000

    Finished counting problem

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Gets user to enter a positive number and counts up to it
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,19 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //gets number from user
+    System.out.println("Please enter a positive integer");
+    int userNum = input.nextInt();
+
+    //for loop to print numbers to screen
+    for(int i = 1; i < userNum; i++){
+      System.out.print(i + ", ");
+    }
+    //prints last number(gets rid of comma at end)
+    System.out.print(userNum);
     
   }
 }
